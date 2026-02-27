9bf6dd2ae0e80c93a06b7ccaefc03678fe9d4c8e
MD. MAHEDI HASAN <223311200@vu.edu.bd>
2026-02-28 00:09:21 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from datetime import date
+from utils import add, subtract
 
 print("Name: MD. MAHEDI HASAN")
 print("Date:", date.today())
+
+print("Add:", add(5, 3))
+print("Subtract:", subtract(5, 3))
